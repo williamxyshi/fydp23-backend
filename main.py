85f7f2bc1a74837c4042d2e46717e637076eec2d
William Shi <45865618+williamxyshi@users.noreply.github.com>
2023-02-02 16:09:15 -0500

    s

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 import datetime
 
 app = FastAPI()
 
+"""
+brew types:
+  - single -> immediately schedule a brew
+  - schedule -> schedule a brew to occur on a set interval
+
+single : starttime -> when it starts (could be now or future)
+         endtime -> when to end the brew starting now
+
+scheduled : starttime -> when it starts (could be now or future)
+            endtime -> when to end the brew starting now
+            interval -> every interval time we start a brew
+
+"""
+
 class CreateBrew(BaseModel):
   type: str
-  starttime: datetime
-  endtime: datetime
-  interval: datetime
+  start_time: datetime
+  end_time: datetime
+  interval: datetime #every x hours
   water_amount: int
   ground_amount: int
 
 class EditBrew(BaseModel):
   id: str
-  starttime: datetime
-  endtime: datetime
+  start_time: datetime
+  end_time: datetime
   interval: datetime
   water_amount: int
   ground_amount: int
@@ -23,20 +37,26 @@ class EditBrew(BaseModel):
 class DeleteBrew(BaseModel):
   id: str
 
+brews = []
+current_brew = {}
+
 #Return all brews
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"data": brews}
 
 #schedule a brew
 @app.post("/")
 async def root(data: CreateBrew):
 
     if data.type == 'single':
-      print()
+      print("single brew")
+      brew = {
+        ""
+      }
 
     elif data.type == 'schedule':
-      print()
+      print("schedule a brew")
 
     return {"message": "Hello World"}
 
