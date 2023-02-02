8aad4eb51089b9c5f34c1be2ed124fec7d2c12cf
George Liu <liugeorge07@gmail.com>
2023-02-02 14:29:51 -0500

    Arduino boilerplate

---
+++ arduino/aghhh/aghhh.ino
@@ -0,0 +1,54 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+// replace the MAC address below by the MAC address printed on a sticker on the Arduino Shield 2
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+EthernetClient client;
+
+int    HTTP_PORT   = 80;
+String HTTP_METHOD = "GET";
+char   HOST_NAME[] = "maker.ifttt.com";
+String PATH_NAME   = "/trigger";
+String queryString = "?value1=26&value2=70";;
+
+void setup() {
+  Serial.begin(9600);
+
+  // initialize the Ethernet shield using DHCP:
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to obtaining an IP address using DHCP");
+    while(true);
+  }
+
+  // connect to web server on port 80:
+  if(client.connect(HOST_NAME, HTTP_PORT)) {
+    // if connected:
+    Serial.println("Connected to server");
+    // make a HTTP request:
+    // send HTTP header
+    client.println(HTTP_METHOD + " " + PATH_NAME + queryString + " HTTP/1.1");
+    client.println("Host: " + String(HOST_NAME));
+    client.println("Connection: close");
+    client.println(); // end HTTP header
+
+    while(client.connected()) {
+      if(client.available()){
+        // read an incoming byte from the server and print it to serial monitor:
+        char c = client.read();
+        Serial.print(c);
+      }
+    }
+
+    // the server's disconnected, stop the client:
+    client.stop();
+    Serial.println();
+    Serial.println("disconnected");
+  } else {// if not connected:
+    Serial.println("connection failed");
+  }
+}
+
+void loop() {
+
+}
