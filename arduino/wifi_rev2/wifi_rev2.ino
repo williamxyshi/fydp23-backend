6527b52e52f599da31006997b55fb6eae64ab915
George Liu <liugeorge07@gmail.com>
2023-02-16 10:36:35 -0500

    Add connection to real host and JSON parsing

--- arduino/wifi_rev2/wifi_rev2.ino
+++ arduino/wifi_rev2/wifi_rev2.ino
@@ -1,9 +1,11 @@
 #include <WiFiNINA.h>
+#include <ArduinoJson.h>
+#include <utility/wifi_drv.h>
 
 char ssid[] = "RogersD6824";
 char pass[] = "082FBRogers";
 
-char host[] = "wifitest.adafruit.com";  // API host name
+char brew_host[] = "fydp.eba-twsqhru6.us-east-1.elasticbeanstalk.com";
 int port = 80;      // port
 
 int status = WL_IDLE_STATUS;      //connection status
@@ -11,12 +13,13 @@ WiFiServer server(80);            //server socket
 
 WiFiClient client = server.available();
 
-int ledPin = 2;
-
 void setup() {
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
   while (!Serial);
+
+  WiFiDrv::pinMode(25, OUTPUT);  //GREEN
+  WiFiDrv::pinMode(26, OUTPUT);  //RED
+  WiFiDrv::pinMode(27, OUTPUT);  //BLUE
   
   enable_WiFi();
   connect_WiFi();
@@ -35,26 +38,67 @@ void setup() {
   Serial.print(rssi);
   Serial.println(" dBm");
 
-  Serial.println("Testing connection");
-  if (client.connect(host, port)) {
-    Serial.print("connected to host: ");
-    Serial.println(host);
+  Serial.println("Connecting to brew daddy");
+  if (client.connect(brew_host, port)) {
+    Serial.print("connected to brew daddy: ");
+    Serial.println(brew_host);
 
-    client.println("GET /testwifi/index.html HTTP/1.1");
-    client.println("Host: wifitest.adafruit.com");
+    client.println("GET / HTTP/1.1");
+    client.println("Host: fydp.eba-twsqhru6.us-east-1.elasticbeanstalk.com");
     client.println("Connection: keep-alive");
     client.println("Accept: */*");
 
     client.println();
+  } else {
+    Serial.println("Connection failed");
   }
-
 }
 
 void loop() {
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
+  String response = "";
+  while (client.available()) {
+    response += char(client.read());
   }
+
+  if (response != "") {
+    Serial.println(response);
+
+    String json_str = skipResponseHeaders(response);
+
+    // JSON doc used to deserialize JSON strings
+    StaticJsonDocument<200> doc;
+
+    DeserializationError error = deserializeJson(doc, json_str);
+
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+    } else {
+      Serial.println("Deserialized doc[data] (json is valid)");
+      WiFiDrv::digitalWrite(27, HIGH);
+      delay(500);
+      WiFiDrv::digitalWrite(26, HIGH);
+      delay(500);
+      WiFiDrv::digitalWrite(25, HIGH);
+      delay(10000);
+      WiFiDrv::digitalWrite(25, LOW);
+      WiFiDrv::digitalWrite(26, LOW);
+      WiFiDrv::digitalWrite(27, LOW);      
+    }
+  }
+
+
+}
+
+String skipResponseHeaders(String response) {
+  int i = response.indexOf("\r\n\r\n");
+  if (i != -1) {
+    String json_str = response.substring(i + 4);
+    Serial.print("Skipping headers, we get: ");
+    Serial.println(json_str);
+    return json_str;   
+  }
+  return "";
 }
 
 void enable_WiFi() {
